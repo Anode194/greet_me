92822f970a31740b7a2cc6d4c2f38708c3b77b5a
Jo Phillips <joxphillips@gmail.com>
2020-03-04 14:37:10 -0700

    implemented the todo struct to store the todos so editings and formatting output will be simpler. added both file paths for the todo file for mac and linux to make working easier on either system.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ use std::process::Command;
 use std::vec::Vec;
 fn main() {
     let joplin_setup = "joplin use TODO";
-    let joplin_setup2 = "joplin ls > $HOME/todo.txt";
+    let joplin_setup2 = "joplin ls > $HOME/.config/greet_me/todo.txt";
+    let mac_todo_filename = "/Users/anode/.config/greet_me/todo.txt";
+    let _linux_todo_filename = "/home/anode/.config/greet_me/todo.txt"; 
 
     let mut setup_1 = Command::new("sh");
     setup_1.arg("-c")
@@ -20,35 +22,43 @@ fn main() {
         .arg(joplin_setup2)
         .output()
         .expect("something went wrong");
-
-    let todo_filename = "/home/anode/todo.txt";
-    let todo_file = File::open(todo_filename).unwrap();
+    //replace with linux when on linux til config file function is built.
+    let todo_file = File::open(mac_todo_filename).unwrap();
     let reader = BufReader::new(todo_file);
 
     let mut todo_text = Vec::new();
     
     println!("welcome Jo here is your todo_list");
-    for (index, line) in reader.lines().enumerate() {
+    for (_index, line) in reader.lines().enumerate() {
         let line = line.unwrap();
         todo_text.push(line);
     }
+    let mut todos = TodoData::new();
+
+    for el in todo_text.iter() {
     let mut todo_string = " ".to_string();
-    let mut todos: Vec<String> = Vec::new();
-    for mut el in todo_text.iter() {
         if el.chars().nth(1) == Some('X') { break;}
-        let mut s = el.clone();
+        let s = el.clone();
         todo_string.push_str(s.trim_start_matches("[ ] ").trim());
         todo_string.push_str(" ");
+        match todo_string.chars().nth(1) {
+            Some('U') => todos.urgent.push(todo_string),
+            Some(_) => todos.non_urgent.push(todo_string),
+            None => println!("an error occured with parsing the todo file"),
+        }
     }
-        println!("{:?}", todo_string);
+        
+        println!("{:?}", todos.urgent);
+        println!("{:?}", todos.non_urgent);
+    //let mut todo_text = Vec::new();
 }
-struct todo {
+struct TodoData {
     urgent: Vec<String>,
     non_urgent: Vec<String>
 }
-impl todos {
-    fn new() -> todo {
-        todo { urgent: Vec::new(), non_urgent: Vec::new()}
+impl TodoData {
+    fn new() -> TodoData {
+        TodoData { urgent: Vec::new(), non_urgent: Vec::new()}
     }
 }
 /* two data structures: urgent todos and not-urgent todos. urgent todo's have U_ 
