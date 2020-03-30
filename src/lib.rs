b3c00da471c5cdf3761541feb7180a5fa2719933
Jo Phillips <joxphillips@gmail.com>
2020-03-29 22:26:00 -0600

    Format output and seperate area of concerns
    removed functions from main file and placed in lib.rs. begun to format
    output so it doesn't look so ugly as it does right now.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use std::process::Command;
 use std::vec::Vec;
-
+use crossterm::style::*;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
 pub struct TodoData {
     pub urgent: Vec<String>,
     pub non_urgent: Vec<String>
@@ -11,7 +14,46 @@ impl TodoData {
     }
 }
 
+pub fn output(todo: TodoData) {
+    for element in todo.urgent.iter() {
+        print!("{}\t",element);
+    }
+        println!(" ");
+        println!(" ");
+    for element in todo.non_urgent.iter() {
+        print!("{}\t",element);
+    }
 
+}
+pub fn read_text_and_parse() -> TodoData {
+    let mac_todo_filename = "/Users/anode/.config/greet_me/todo.txt";
+    let _linux_todo_filename = "/home/anode/.config/greet_me/todo.txt"; 
+
+    //replace with linux when on linux til config file function is built.
+    let todo_file = File::open(mac_todo_filename).unwrap();
+    let reader = BufReader::new(todo_file);
+
+    let mut todo_text = Vec::new();
+    let mut todos = TodoData::new();
+    
+    for (_index, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        todo_text.push(line);
+    }
+        for el in todo_text.iter() {
+        let mut todo_string = " ".to_string();
+        if el.chars().nth(1) == Some('X') { break;}
+        let s = el.clone();
+        todo_string.push_str(s.trim_start_matches("[ ] ").trim());
+        todo_string.push_str(" ");
+        match todo_string.chars().nth(1) {
+            Some('U') => todos.urgent.push(todo_string),
+            Some(_) => todos.non_urgent.push(todo_string),
+            None => println!("an error occured with parsing the todo file"),
+        }
+    }
+        todos
+}
 
 /* two data structures: urgent todos and not-urgent todos. urgent todo's have U_ 
   in front of their name. urgents if they are below 5 are all printed to the 
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,7 @@
-use std::fs::File;
-use std::io::BufRead;
-use std::io::BufReader;
 use greetme::TodoData;
 fn main() {
     greetme::joplin_setup();
-    read_text_and_parse();
-    println!("welcome Jo here is your todo_list");
-    
-        
-    //let mut todo_text = Vec::new();
-}
-fn read_text_and_parse() {
-    let _mac_todo_filename = "/Users/anode/.config/greet_me/todo.txt";
-    let linux_todo_filename = "/home/anode/.config/greet_me/todo.txt"; 
-
-    //replace with linux when on linux til config file function is built.
-    let todo_file = File::open(linux_todo_filename).unwrap();
-    let reader = BufReader::new(todo_file);
-
-    let mut todo_text = Vec::new();
-    let mut todos = TodoData::new();
-    
-    for (_index, line) in reader.lines().enumerate() {
-        let line = line.unwrap();
-        todo_text.push(line);
-    }
-        for el in todo_text.iter() {
-        let mut todo_string = " ".to_string();
-        if el.chars().nth(1) == Some('X') { break;}
-        let s = el.clone();
-        todo_string.push_str(s.trim_start_matches("[ ] ").trim());
-        todo_string.push_str(" ");
-        match todo_string.chars().nth(1) {
-            Some('U') => todos.urgent.push(todo_string),
-            Some(_) => todos.non_urgent.push(todo_string),
-            None => println!("an error occured with parsing the todo file"),
-        }
-    }
-        output(todos);
-}
-fn output(todo: TodoData) {
-    println!("{:?}", todo.urgent);
-    println!("{:?}", todo.non_urgent);
-
+    let output_text = greetme::read_text_and_parse();
+    println!("\t\twelcome Jo here is your todo_list");
+    greetme::output(output_text)
 }
