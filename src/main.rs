4899eab0170d3b8caa998b906b4386b58a321524
Jo Phillips <joxphillips@gmail.com>
2020-10-29 18:07:55 -0600

    Command Parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
+mod data;
+mod input;
+mod output;
+
+pub use crate::data::*;
+pub use crate::input::*;
+pub use crate::output::*;
+use std::env;
+
 fn main() {
-    greetme::joplin_setup();
-    let output_text = greetme::read_text_and_parse();
-    greetme::output(output_text)
+    let args: Vec<String> = env::args().collect();
+    match args[1].as_ref() {
+        "-a" => {
+            let mut output_text = read_alt_format(&args[2]);
+            output::output(output_text)
+        }
+        "-t" => {}
+        "-h" | "--help" => {
+            println!(" greet me is a simple todo tracker / greater");
+            println!("-a 'file name'\t\t for todo lists not tracked with joplin");
+            println!("-h --help \t\t'prints this help message'");
+        }
+        _ => {
+            input::joplin_setup();
+            let output_text = read_text_and_parse();
+            output::output(output_text)
+        }
+    }
 }
